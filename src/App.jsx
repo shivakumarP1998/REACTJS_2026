5f1390686b5f6021dc9692124cb9c37e235019e9
P.shivakumar <60265650+shivakumar1998@users.noreply.github.com>
2026-01-28 14:19:09 +0530

    custom component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+import configImg from "./assets/config.png";
+import jsxImg from "./assets/jsx-ui.png";
+import stateImg from "./assets/state-mgmt.png";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -20,11 +24,45 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI Building Block."
+              image={componentsImg}
+            />
+            <CoreConcept
+              title="Props"
+              description="The core UI Building Block."
+              image={configImg}
+            />
+            <CoreConcept
+              title="JSX"
+              description="The core UI Building Block."
+              image={jsxImg}
+            />
+            <CoreConcept
+              title="State"
+              description="The core UI Building Block."
+              image={stateImg}
+            />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
