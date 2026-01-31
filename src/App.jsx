6dc4fb8f1153e5b0cc25a414bd8cefd214e58133
P.shivakumar <60265650+shivakumar1998@users.noreply.github.com>
2026-01-31 20:07:40 +0530

    session58

--- src/App.jsx
+++ src/App.jsx
@@ -33,15 +33,18 @@ function App() {
       <main>
         <section id="core-concepts">
           <ul>
-            <CoreConcept
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
+            {/* <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
             {/* shortcut use or alternative */}
-            <CoreConcept {...CORE_CONCEPTS[1]} />
+            {/* <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
           </ul>
         </section>
         <section id="examples">
