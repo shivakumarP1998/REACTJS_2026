8fd219386e95b71a3bf49aeca26933a279af408b
P.shivakumar <60265650+shivakumar1998@users.noreply.github.com>
2026-02-04 13:48:54 +0530

    Session69

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -30,6 +30,8 @@ export default function Examples() {
   return (
     <Section title="Examples" id="examples">
       <Tabs
+        // ButtonsContainer={Section} // Custom components
+        ButtonsContainer="menu" // built-in components
         buttons={
           <>
             <TabButton
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,9 +1,10 @@
 import { Children } from "react";
 
-export default function Tabs({ children, buttons }) {
+export default function Tabs({ children, buttons, ButtonsContainer }) {
+  //   const ButtonsContainer = buttonsContainer;
   return (
     <>
-      <menu>{buttons}</menu>
+      <ButtonsContainer>{buttons}</ButtonsContainer>
       {children}
     </>
   );
