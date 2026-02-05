09a851fbd6b3c53707ad307bba285fbf6e40cd62
P.shivakumar <60265650+shivakumar1998@users.noreply.github.com>
2026-02-05 13:36:35 +0530

    Session75

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import Player from "./components/Player";
+
 function App() {
-  return <P>Coming soon...</P>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player1" symbol="X" />
+          <Player name="Player2" symbol="O" />
+        </ol>
+      </div>
+    </main>
+  );
 }
 
 export default App;
